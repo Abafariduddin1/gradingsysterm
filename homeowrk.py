2022fd92a72b2d6a0106f235e3fd3d7f0f902bb5
Abafariduddin1 <55846115+Abafariduddin1@users.noreply.github.com>
2024-11-02 14:37:01 +0000

    Add files via upload

---
+++ homeowrk.py
@@ -0,0 +1,14 @@
+
+marks = int(input("Enter the marks scored in the exam: "))
+
+
+if marks >= 90:
+    print("Your grade is : A")
+elif marks >= 75:
+    print("Your grade is : B")
+elif marks >= 60:
+    print("Your grade is : C")
+elif marks >= 45:
+    print("Your grade is : D")
+else:
+    print("Your grade is : Fail")
